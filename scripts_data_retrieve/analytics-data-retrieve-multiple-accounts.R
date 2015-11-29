02568ee756b99f08d08fd71b567bd741525e2098
terranova83 <sylvain.le.barillec@gmail.com>
2015-11-29 17:09:44 +0100

    Add One Function to retrieve the GA View ID in the export

--- scripts_data_retrieve/analytics-data-retrieve-multiple-accounts.R
+++ scripts_data_retrieve/analytics-data-retrieve-multiple-accounts.R
@@ -1,50 +1,46 @@
 setwd("/Users/sylvain/Desktop") #set work directory
 getwd() #check work directory
 require(RGoogleAnalytics) #load RGoogleAnalytics library
-fix(GetDataFeed)
-GetDataFeed <- function(query.uri) {
-  
-  GA.Data <- GET(query.uri)
-  GA.list <- ParseDataFeedJSON(GA.Data)
-  if (is.null(GA.list$rows)) {
-    cat("Your query matched 0 results. Please verify your query using the Query Feed Explorer and re-run it.\n")
-    # break
-    return(GA.list)
-  } else {
-    return (GA.list)
-  }
-}
-
 # Authorize the Google Analytics account
 # This need not be executed in every session once the token object is created 
 # and saved
 
+#client.id<-""
+#client.secret<-""
 #token <- Auth(client.id,client.secret)
 
 # Save the token object for future sessions
-#save(token,file="token_file")
+#save(token,file="token")
 
 # In future sessions it can be loaded by running load("token")
-load("token_file")
+load("token")
 
 ValidateToken(token)
 
 viewID<-GetProfiles(token)
 viewID
-category_1<- c("13708799", "39852040")
-for (v in category_1){
-  start.date <- "2014-04-01"
-  end.date <- "2014-04-30"
-  view.id <- paste("ga:",v,sep="") #the View ID parameter need to have "ga:" in front of the ID 
-  
-  query.list <- Init(start.date = start.date, end.date = end.date, dimensions = "ga:date,ga:deviceCategory", metrics = "ga:sessions, ga:users, ga:bounceRate", table.id = view.id, max.results = 10000)
-  ga.query <- QueryBuilder(query.list)
 
-   ga.data <- GetReportData(ga.query, token, paginate_query = F,split_daywise = F)
- # class(ga.data$date) # dates come as character
- # newDate<-as.Date(ga.data$date,"%Y%M%d")  #convert into date data type
- # newFormat<- format(newDate,"%m/%d/%y") #to change format, but it convets it back to character class
- # newFormat<- as.Date(newFormat,"%d/%m/%y")  #convert it back to date data type
-  df<-rbind(df,ga.data)
+df<-data.frame()
+analyticsviewID<-data.frame() #initialize frame to add one column in the CSV with the analytics View ID
+for (v in c("13708799","39852040")){ #GA accounts
+  start.date <- "2014-04-01"
+  end.date <- "2014-04-05"
+  view.id <- paste("ga:",v,sep="") #the View ID parameter needs to have "ga:" in front of the ID 
+  query.list <- Init(start.date = start.date, 
+                     end.date = end.date, 
+                     dimensions = "ga:date,ga:deviceCategory", 
+                     metrics = "ga:sessions, ga:users", 
+                     table.id = view.id, 
+                     max.results = 10000)
+  ga.query <- QueryBuilder(query.list)
+  ga.data <- GetReportData(ga.query, token,split_daywise = T,paginate_query = F)
+
+analyticsviewID<-cbind(ga.data,v)
+df<-rbind(df,analyticsviewID)
+
 }
-write.csv(df, "google_analytics_API-Q.csv")
+class(df$date)
+newDate<-as.Date(df$date,"%Y%M%d")  #convert into date data type
+newFormat<- format(newDate,"%m/%d/%y") #to change format, but it convets it back to character class
+newFormat<- as.Date(newFormat,"%d/%m/%y")  #convert it back to date data type
+write.csv(df, "google_analytics.csv")
